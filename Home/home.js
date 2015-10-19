f87649d7b6edb9e4f307748122f8dc24875fa60c
Administrator <admin@example.com>
2015-10-19 18:11:14 -0500

    angularjs testing demo

---
+++ Home/home.js
@@ -0,0 +1,13 @@
+'use strict';
+ 
+var app = angular.module('Application', ['ngResource']);
+ 
+app.factory('UserFactory', function($resource){
+    return $resource('Users/users.json')
+});
+ 
+app.controller('MainCtrl', function($scope, UserFactory) {
+    $scope.text = 'Hello World!';
+    $scope.text2 = 'eric';
+    $scope.users = UserFactory.get();
+});
